30a809fdbd1fce362790ca41e9e962b04998d246
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-12-16 16:11:14 +0400

    end pay

---
+++ agg/assets/js/customSelect.js
@@ -0,0 +1,42 @@
+let selectTitle = document.querySelectorAll('.custom__select-title')
+let selectContent = document.querySelectorAll('.custom__select-content')
+let selectText = document.querySelectorAll('.custom__select-text')
+let selectCustom = document.querySelectorAll('.custom__select')
+
+selectContent.forEach((content, contentIndex) => {
+  content.addEventListener('click', function(e) {
+    if(e.target != this){
+      selectTitle.forEach((title, titleIndex) => {
+        if(titleIndex == contentIndex){
+          title.textContent = e.target.textContent
+        }
+      })
+      selectCustom.forEach((select) => {
+        select.classList.remove('custom__select--active')
+      })
+      content.classList.toggle('hide')
+    }
+  })
+})
+
+selectTitle.forEach((title, titleIndex) => {
+  title.addEventListener('click', function(){
+    selectContent.forEach((content, contentIndex) => {
+      if(titleIndex == contentIndex){
+        content.classList.toggle('hide')
+      }
+      else{
+        content.classList.add('hide')
+      }
+    })
+    selectCustom.forEach((select, selectIndex) => {
+      if(titleIndex == selectIndex){
+        select.classList.toggle('custom__select--active')
+      }
+      else{
+        select.classList.remove('custom__select--active')
+      }
+    })
+  })
+})
+
