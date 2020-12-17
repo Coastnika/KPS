c8c72673750e7598674c2137a9366bf2302a5cb4
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-12-17 16:27:08 +0400

    pravki

--- agg/assets/js/dropdown.js
+++ agg/assets/js/dropdown.js
@@ -2,8 +2,21 @@ let dropDownToggle = document.querySelectorAll('.dropdown-active')
 let dropDownElement = document.querySelectorAll('.dropdown-element')
 let contract = document.querySelector('.form__payer-contract')
 let closeForm1 = document.querySelector('.addcompany__title-close')
-let closeForm2 = document.querySelector('.button-close')
+// let closeForm2 = document.querySelector('.button-close')
 let tabArrows = document.querySelectorAll('.settings__dropdown-arrow');
+let saveForm = document.querySelectorAll('.save-form')
+
+if(saveForm){
+  saveForm.forEach(save => {
+    save.addEventListener('click', ()=> {
+      dropDownElement.forEach(element => {
+        if(save.getAttribute('tab-index') == element.getAttribute('tab-index')){
+          element.classList.toggle('hide')
+        }
+      })
+    })
+  })
+}
 
 dropDownToggle.forEach(toggle => {
   toggle.addEventListener('click', () =>{
@@ -39,16 +52,16 @@ closeForm1.addEventListener('click', () => {
   })
 })
 
-closeForm2.addEventListener('click', () => {
-  dropDownElement.forEach(element => {
-    if(element.getAttribute('tab-index') == closeForm2.getAttribute('tab-index')){
-      element.classList.toggle('hide')
-      contract.classList.toggle('hide')
-    }
-  })
-  tabArrows.forEach(arrow => {
-    if(closeForm1.getAttribute('tab-index') == arrow.getAttribute('tab-index')){
-      arrow.classList.toggle('settings__dropdown-arrow--active')
-    }
-  })
-})
+// closeForm2.addEventListener('click', () => {
+//   dropDownElement.forEach(element => {
+//     if(element.getAttribute('tab-index') == closeForm2.getAttribute('tab-index')){
+//       element.classList.toggle('hide')
+//       contract.classList.toggle('hide')
+//     }
+//   })
+//   tabArrows.forEach(arrow => {
+//     if(closeForm1.getAttribute('tab-index') == arrow.getAttribute('tab-index')){
+//       arrow.classList.toggle('settings__dropdown-arrow--active')
+//     }
+//   })
+// })
--- agg/assets/js/showForm.js
+++ agg/assets/js/showForm.js
@@ -2,7 +2,7 @@ let settingsForm = document.querySelector('.addcompany')
 let addOrganization = document.querySelector('.form__payer-btn')
 let lastControls = document.querySelector('.settings__payer-controlls-last')
 let firstControls = document.querySelector('.settings__payer-controlls-first')
-let buttonClose = document.querySelector('.button-close')
+// let buttonClose = document.querySelector('.button-close')
 let buttonClose2 = document.querySelector('.addcompany__title-close')
 let contractCheckbox = document.querySelector('.form__payer-contract')
 
@@ -13,6 +13,7 @@ function hideForm(){
   addOrganization.classList.remove('hide')
   firstControls.classList.remove('hide')
   contractCheckbox.classList.remove('form__payer-contract--form')
+  contractCheckbox.classList.add('hide')
 }
 
 function showForm(){
@@ -21,9 +22,12 @@ function showForm(){
   addOrganization.classList.add('hide')
   firstControls.classList.add('hide')
   contractCheckbox.classList.add('form__payer-contract--form')
+  contractCheckbox.classList.remove('hide')
 }
 
 
 addOrganization.addEventListener('click', showForm)
-buttonClose.addEventListener('click', hideForm)
+// buttonClose.addEventListener('click', hideForm)
 buttonClose2.addEventListener('click', hideForm)
+
+
