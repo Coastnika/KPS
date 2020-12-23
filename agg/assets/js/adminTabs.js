a6c9ef8f52eca8e54869f4a55c6f4cceed5ca530
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-12-24 02:53:48 +0400

    pravki ot veroniki

--- agg/assets/js/adminTabs.js
+++ agg/assets/js/adminTabs.js
@@ -7,13 +7,22 @@ cards.forEach(card => {
   card.addEventListener('click', e => {
     let toggleContent = card.querySelectorAll('.admin-wrapper')
     let target = e.target.closest('.admin__nav-item')
+    let navItem = document.querySelectorAll('.admin__nav-item')
 
+    
+    
     toggleContent.forEach(content => {
       if(target.getAttribute('tab-index') == content.getAttribute('tab-index')){
+        
+        navItem.forEach(item => {
+          item.classList.remove('admin__nav-item-active')
+        })
+        target.classList.add('admin__nav-item-active')
         content.classList.remove('hide')
       }
       else if(target.getAttribute('tab-index') != content.getAttribute('tab-index')){
         content.classList.add('hide')
+        
       }
     })  
   })
