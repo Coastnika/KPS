546a37dc30e33d8bce41d8b5754fa16f9ef5a638
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-12-25 13:59:40 +0400

    fix statstics

--- agg/assets/js/adminTabs.js
+++ agg/assets/js/adminTabs.js
@@ -1,19 +1,23 @@
 let cards = document.querySelectorAll('.admin__card')
-// let toggleTab = document.querySelectorAll('.admin__nav-item')
+let navItems = document.querySelectorAll('.admin__nav-item')
+let navContainer = document.querySelector('.admin__card-nav')
 let toggleContent = document.querySelector('.admin-wrapper')
 
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navContainer.classList.add('admin__card-nav--border')
+  })
+})
 
+//Показывает табы 
 cards.forEach(card => {
   card.addEventListener('click', e => {
     let toggleContent = card.querySelectorAll('.admin-wrapper')
     let target = e.target.closest('.admin__nav-item')
     let navItem = document.querySelectorAll('.admin__nav-item')
 
-    
-    
     toggleContent.forEach(content => {
       if(target.getAttribute('tab-index') == content.getAttribute('tab-index')){
-        
         navItem.forEach(item => {
           item.classList.remove('admin__nav-item-active')
         })
@@ -22,7 +26,6 @@ cards.forEach(card => {
       }
       else if(target.getAttribute('tab-index') != content.getAttribute('tab-index')){
         content.classList.add('hide')
-        
       }
     })  
   })
--- agg/assets/js/dropdownCompany.js
+++ agg/assets/js/dropdownCompany.js
@@ -1,11 +1,15 @@
 let dropdownToggle = document.querySelector('.dropdown-checkbox__toggle')
 let dropdownContainer = document.querySelector('.dropdown-checkbox__container')
 let dropdownClose = document.querySelector('.dropdown-checkbox__close')
+let dropdownSave = document.querySelector('.company-save')
 
 dropdownToggle.addEventListener('click', () => {
   dropdownContainer.classList.toggle('hide')
 })
 dropdownClose.addEventListener('click', () => {
+  dropdownContainer.classList.add('hide')
+})
+dropdownSave.addEventListener('click', () => {
   dropdownContainer.classList.add('hide')
 })
 
