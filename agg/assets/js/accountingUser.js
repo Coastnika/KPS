5e73b7cfc5d49d2f8cba67866bd82ef1f8be1bae
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-12-28 16:37:12 +0400

    123

---
+++ agg/assets/js/accountingUser.js
@@ -0,0 +1,70 @@
+// //validation contacts
+let accountingInputs = document.querySelectorAll('.accounting__contacts-input')
+let buttonSend = document.querySelector('.submit-disable')
+let accountingForm = document.querySelector('.accounting__contacts-form')
+
+
+accountingForm.addEventListener('input', () => {
+  let valid = 0;
+
+  accountingInputs.forEach(input => {
+    if(input.value.trim() != ''){
+      valid = 1
+    }
+    else return
+    
+    console.log(valid)
+  })
+  if(valid == 1){
+    buttonSend.classList.add('submit-disable-hide')
+  }
+  
+
+})
+
+buttonSend.addEventListener('click', e => {
+  e.preventDefault()
+})
+
+// logic 
+let accountingButton = document.querySelector('.accounting-button')
+let question = document.querySelector('.accounting__question')
+let questionAccept = document.querySelector('.accounting__question-accept')
+let questionDecline = document.querySelector('.accounting__question-decline')
+let sendAddress = document.querySelector('.accounting__send')
+let accountingContacts = document.querySelector('.accounting__contacts')
+let accountingSeccessful = document.querySelector('.accounting__successful')
+let getOriginal = document.querySelector('.get-original')
+
+//1
+accountingButton.addEventListener('click', function(){
+  toggleContent(this, question)
+})
+//2
+questionAccept.addEventListener('click', function() {
+  toggleContent(question, sendAddress)
+  asyncRequire(sendAddress, accountingSeccessful)
+})
+//3
+questionDecline.addEventListener('click', function() {
+  toggleContent(question, accountingContacts)
+})
+//4
+getOriginal.addEventListener('click', () => {
+  toggleContent(accountingContacts, sendAddress)
+  asyncRequire(sendAddress, accountingSeccessful)
+})
+
+
+function toggleContent(hide, show){
+  hide.classList.add('hide')
+  show.classList.remove('hide')
+}
+
+function asyncRequire(hide, show) {
+  setTimeout(() => {
+    hide.classList.add('hide')
+    show.classList.remove('hide')
+  }, 2000)
+}
+
