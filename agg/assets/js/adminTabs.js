00aa668b9313654d6f85c1c57b965e1a303a482c
AlexanderKhabarov-dev <sanek737373@inbox.ru>
2020-12-28 14:31:02 +0400

    fix admins tab

--- agg/assets/js/adminTabs.js
+++ agg/assets/js/adminTabs.js
@@ -1,11 +1,11 @@
 let cards = document.querySelectorAll('.admin__card')
 let navItems = document.querySelectorAll('.admin__nav-item')
-let navContainer = document.querySelector('.admin__card-nav')
+
 let toggleContent = document.querySelector('.admin-wrapper')
 
 navItems.forEach(item => {
   item.addEventListener('click', () => {
-    navContainer.classList.add('admin__card-nav--border')
+    
   })
 })
 
@@ -14,7 +14,8 @@ cards.forEach(card => {
   card.addEventListener('click', e => {
     let toggleContent = card.querySelectorAll('.admin-wrapper')
     let target = e.target.closest('.admin__nav-item')
-    let navItem = document.querySelectorAll('.admin__nav-item')
+    let navItem = card.querySelectorAll('.admin__nav-item')
+    let navContainer = card.querySelector('.admin__card-nav')
 
     toggleContent.forEach(content => {
       if(target.getAttribute('tab-index') == content.getAttribute('tab-index')){
@@ -24,9 +25,11 @@ cards.forEach(card => {
             item.classList.remove('admin__nav-item-active')
           })
           target.classList.add('admin__nav-item-active')
+          navContainer.classList.add('admin__card-nav--border')
         }
         else if(target.classList.contains('admin__nav-item-active')){
           target.classList.remove('admin__nav-item-active')
+          navContainer.classList.remove('admin__card-nav--border')
         }
       }
       if(target.getAttribute('tab-index') != content.getAttribute('tab-index')){
